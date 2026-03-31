c8f80cbbdad10f3d59adcd75ad72cbb09df95032
Ryan Emmick <ryanemmick4@gmail.com>
2026-03-31 09:59:45 -0500

    Increase nickname input text size to text-2xl

--- packages/client/src/screens/LandingScreen.tsx
+++ packages/client/src/screens/LandingScreen.tsx
@@ -75,7 +75,7 @@ export function LandingScreen() {
             value={nickname}
             onChange={(e) => setNickname(e.target.value)}
             maxLength={20}
-            className="w-full px-4 py-3 bg-transparent border-b border-gray-600 text-white text-center text-lg focus:outline-none focus:border-white"
+            className="w-full px-4 py-3 bg-transparent border-b border-gray-600 text-white text-center text-2xl focus:outline-none focus:border-white"
             style={{ fontFamily: 'UnoHandwritten, sans-serif' }}
           />
           {!nickname && (
